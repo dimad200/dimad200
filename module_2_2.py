5dda6c2c8b98f7ad4b1fa063e76878f325d3e5fa
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-10-24 07:27:30 +0000

    123

--- module_2_2.py
+++ module_2_2.py
@@ -1,3 +1,9 @@
 first = input("Введи первое число: ")
 second = input("Введи второе число: ")
 third = input("Введи третье число: ")
+if (first == second) and (second == third):
+    print("Результат: 3")
+elif (first == third) or (second == third) or (first==second):
+    print("Результат: 2")
+else:
+    print("Результат: 0")
