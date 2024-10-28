5f7e2b9f8d62659f82d27fcdf951af54076ec599
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-10-28 10:57:30 +0000

    Финальная домашка по модулю 2

---
+++ modulr_2_hard.py
@@ -0,0 +1,25 @@
+print ("Введите выпавшее число")
+number=int(input())
+Spisok_dlya_perebora=[]
+result=[]
+# Создам список для перебора
+
+for i in range(number-1):
+  Spisok_dlya_perebora.append(i+1)
+# Создам список c результами преобразований 
+for i in range(len(Spisok_dlya_perebora)):
+  Spisok_vnutrenniy = Spisok_dlya_perebora.copy()
+  a=Spisok_vnutrenniy.pop(i)
+  for k in range(len(Spisok_vnutrenniy)):
+    b=Spisok_vnutrenniy[k]    
+    if number%(a+b)==0:
+      c=(a in result) and (b in result)      
+      if c==False:
+        result.append(a)
+        result.append(b)
+# Приведу результат к виду, который указан в задании
+result_str=""
+print("result=",result)
+for i in range(len(result)):
+  result_str=result_str+str(result[i])
+print (result_str)
